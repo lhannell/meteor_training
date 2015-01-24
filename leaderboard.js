228f893854057db1b0e9a8f01db06293cdab3326
Lucas Hannell <lucashannell@gmail.com>
2015-01-24 16:11:04 +0900

    advancing to Chapter 8

--- leaderboard.js
+++ leaderboard.js
@@ -4,7 +4,8 @@ PlayersList = new Meteor.Collection('players');
 if (Meteor.isClient) {
   
   Template.leaderboard.player = function() {
-    return PlayersList.find();
+    return PlayersList.find({},{sort: {score: -1, name: 1}});
+
   };
 
   Template.leaderboard.selectedClass = function (){
@@ -15,13 +16,43 @@ if (Meteor.isClient) {
     };
   };
 
+  Template.leaderboard.showSelectedPlayer = function () {
+    var selectedPlayer = Session.get('selectedPlayer');
+    return PlayersList.findOne(selectedPlayer);
+  };
+
   Template.leaderboard.events ({
-      'mouseover li.player': function() {
+      'click li.player': function() {
         var playerId = this._id;
         Session.set('selectedPlayer', playerId);
         var selectedPlayer = Session.get('selectedPlayer');
-        console.log(selectedPlayer);
+          console.log(selectedPlayer);
+      },
+      'click #increment': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update({_id: selectedPlayer}, {$inc:{score: 5}});
+      },
+      'click #remove': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.remove(selectedPlayer);
+      },
+      'click #decrement': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update({_id: selectedPlayer}, {$inc:{score: -5}});
       }
+
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function(event, template) {
+      event.preventDefault();
+      var playerNameVar = template.find('#playerName').value;
+      PlayersList.insert({
+        name: playerNameVar,
+        score:0
+      });
+      console.log(playerNameVar);
+    }
 
   });
 
